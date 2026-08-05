7ed80d7b4b0188deee9c9fd2aae7439c87407532
DR1987-dev <108408607+DR1987-dev@users.noreply.github.com>
2026-08-05 06:10:09 +0200

    runAll.js aktualisieren

--- runAll.js
+++ runAll.js
@@ -1,12 +1,12 @@
 function runAll() {
   try {
     importAllCSVFromDrive();
-    generateAlleBuchungenPlan();
-    generateMonatswerte();
     importLodgifyEinnahmenToImport();
     importLexwareAll();
     processLodgifyInvoiceUploadToLexware();
     createLexwareFixkosten();
+    generateAlleBuchungenPlan();
+    generateMonatswerte();
     Logger.log("✅ Alle Skripte erfolgreich ausgeführt");
   } catch (e) {
     Logger.log("❌ Abbruch wegen Fehler: " + e.message);
